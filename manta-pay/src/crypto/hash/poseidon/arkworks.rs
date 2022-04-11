f0a5fd664f8a3c0f8ac6079483647c0792e5552d
Conghao Shen <me@tomshen.io>
2022-04-10 23:51:01 -0700

    adapt grainLFSR and round constants from plonk prototype

--- manta-pay/src/crypto/hash/poseidon/arkworks.rs
+++ manta-pay/src/crypto/hash/poseidon/arkworks.rs
@@ -1,8 +1,10 @@
-use ark_ff::{Field};
-use ark_std::{One, Zero};
 use crate::crypto::hash::poseidon::constants::ParamField;
+use ark_ff::{BigInteger, Field, FpParameters, PrimeField};
+use ark_std::{One, Zero};
 
 impl ParamField for ark_bls12_381::Fr {
+    const MODULUS_BITS: usize = <Self as PrimeField>::Params::MODULUS_BITS as usize;
+
     fn zero() -> Self {
         <Self as Zero>::zero()
     }
@@ -13,5 +15,14 @@ impl ParamField for ark_bls12_381::Fr {
 
     fn inverse(&self) -> Option<Self> {
         <Self as Field>::inverse(self)
+    }
+
+    fn try_from_bits_le(bits: &[bool]) -> Option<Self> {
+        let bigint = <Self as PrimeField>::BigInt::from_bits_le(&bits);
+        Self::from_repr(bigint)
+    }
+
+    fn from_le_bytes_mod_order(bytes: &[u8]) -> Self {
+        <Self as PrimeField>::from_le_bytes_mod_order(bytes)
     }
 }
--- manta-pay/src/crypto/hash/poseidon/constants.rs
+++ manta-pay/src/crypto/hash/poseidon/constants.rs
@@ -14,9 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with manta-rs.  If not, see <http://www.gnu.org/licenses/>.
 
+use crate::crypto::hash::poseidon::matrix::{Matrix, MdsMatrices, SparseMatrix};
 use core::fmt::Debug;
 use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
-use crate::crypto::hash::poseidon::matrix::{Matrix, MdsMatrices, SparseMatrix};
 
 /// TODO doc
 pub trait ParamField:
@@ -25,20 +25,29 @@ pub trait ParamField:
     + PartialEq
     + Eq
     + Debug
-    + Add<Output=Self>
+    + Add<Output = Self>
     + for<'a> AddAssign<&'a Self>
-    + Mul<Output=Self>
+    + Mul<Output = Self>
     + for<'a> MulAssign<&'a Self>
-    + Sub<Output=Self>
+    + Sub<Output = Self>
     + for<'a> SubAssign<&'a Self>
     + From<u64>
 {
+    /// Number of bits of modulus of the field.
+    const MODULUS_BITS: usize;
+
     /// TODO doc
     fn zero() -> Self;
     /// TODO doc
     fn one() -> Self;
     /// TODO doc
     fn inverse(&self) -> Option<Self>;
+
+    /// Convert from bits in little endian order. Return None if bits is out of range.
+    fn try_from_bits_le(bits: &[bool]) -> Option<Self>;
+
+    /// Convert from bytes in little endian order. If the number of bytes is out of range, the result will be modulo.   
+    fn from_le_bytes_mod_order(bytes: &[u8]) -> Self;
 }
 
 #[derive(Clone, Debug, PartialEq, Default)]
@@ -62,4 +71,310 @@ pub struct PoseidonConstants<F: ParamField> {
     pub half_full_rounds: usize,
     /// TODO doc
     pub partial_rounds: usize,
+}
+
+mod lfsr {
+    use crate::crypto::hash::poseidon::constants::ParamField;
+
+    /// LFSR for randomness in Poseidon round constants.
+    // adapted from: https://github.com/arkworks-rs/sponge/blob/51d6fc9aac1fa69f44a04839202b5de828584ed8/src/poseidon/grain_lfsr.rs
+    pub(crate) struct GrainLFSR {
+        state: [bool; 80],
+        prime_num_bits: u64,
+        head: usize,
+    }
+
+    fn append_bits<T: Into<u128>>(state: &mut [bool; 80], head: &mut usize, n: usize, from: T) {
+        let val = from.into() as u128;
+        for i in (0..n).rev() {
+            state[*head] = (val >> i) & 1 != 0;
+            *head += 1;
+            *head %= 80;
+        }
+    }
+
+    impl GrainLFSR {
+        /// Create a new LFSR with given parameters: prime number bits, width of the state, number of full rounds, and number of partial rounds.
+        pub(crate) fn new(prime_num_bits: u64, width: usize, r_f: usize, r_p: usize) -> Self {
+            let mut init_sequence = [false; 80];
+            let mut head = 0;
+            // b0, b1 describes the field
+            append_bits(&mut init_sequence, &mut head, 2, 1u8);
+            // b2...=b5 describes s-box: we always use non-inverse s-box
+            append_bits(&mut init_sequence, &mut head, 4, 0b00000u8);
+            // b6...=b17 describes prime_num_bits
+            append_bits(&mut init_sequence, &mut head, 12, prime_num_bits);
+            // b18...=b29 describes width
+            append_bits(&mut init_sequence, &mut head, 12, width as u16);
+            // b30..=39 describes r_f (num_full_rounds)
+            append_bits(&mut init_sequence, &mut head, 10, r_f as u16);
+            // b40..=49 describes r_p (num_partial_rounds)
+            append_bits(&mut init_sequence, &mut head, 10, r_p as u16);
+            // b50..=79 describes the constant 1
+            append_bits(
+                &mut init_sequence,
+                &mut head,
+                30,
+                0b111111111111111111111111111111u128,
+            );
+            let mut res = GrainLFSR {
+                state: init_sequence,
+                prime_num_bits,
+                head,
+            };
+            res.init();
+            res
+        }
+
+        fn update(&mut self) -> bool {
+            let new_bit = self.bit(62)
+                ^ self.bit(51)
+                ^ self.bit(38)
+                ^ self.bit(23)
+                ^ self.bit(13)
+                ^ self.bit(0);
+            self.state[self.head] = new_bit;
+            self.head += 1;
+            self.head %= 80;
+            new_bit
+        }
+
+        fn init(&mut self) {
+            for _ in 0..160 {
+                self.update();
+            }
+        }
+
+        #[inline]
+        fn bit(&self, index: usize) -> bool {
+            self.state[(index + self.head) % 80]
+        }
+
+        /// TODO: doc
+        pub fn get_bits(&mut self, num_bits: usize) -> Vec<bool> {
+            let mut res = Vec::new();
+
+            for _ in 0..num_bits {
+                // Obtain the first bit
+                let mut new_bit = self.update();
+
+                // Loop until the first bit is true
+                while new_bit == false {
+                    // Discard the second bit
+                    let _ = self.update();
+                    // Obtain another first bit
+                    new_bit = self.update();
+                }
+
+                // Obtain the second bit
+                res.push(self.update());
+            }
+
+            res
+        }
+
+        /// TODO: doc
+        pub fn get_field_elements_rejection_sampling<F: ParamField>(
+            &mut self,
+            num_elems: usize,
+        ) -> Vec<F> {
+            assert_eq!(F::MODULUS_BITS as u64, self.prime_num_bits);
+
+            let mut res = Vec::new();
+            for _ in 0..num_elems {
+                // Perform rejection sampling
+                loop {
+                    // Obtain n bits and make it most-significant-bit first
+                    let mut bits = self.get_bits(self.prime_num_bits as usize);
+                    bits.reverse();
+
+                    // Construct the number
+                    if let Some(f) = F::try_from_bits_le(&bits) {
+                        res.push(f);
+                        break;
+                    }
+                }
+            }
+
+            res
+        }
+
+        /// TODO: doc
+        pub fn get_field_elements_mod_p<F: ParamField>(&mut self, num_elems: usize) -> Vec<F> {
+            assert_eq!(F::MODULUS_BITS as u64, self.prime_num_bits);
+
+            let mut res = Vec::new();
+            for _ in 0..num_elems {
+                // Obtain n bits and make it most-significant-bit first
+                let mut bits = self.get_bits(self.prime_num_bits as usize);
+                bits.reverse();
+
+                let bytes = bits
+                    .chunks(8)
+                    .map(|chunk| {
+                        let mut result = 0u8;
+                        for (i, bit) in chunk.iter().enumerate() {
+                            result |= u8::from(*bit) << i
+                        }
+                        result
+                    })
+                    .collect::<Vec<u8>>();
+
+                res.push(F::from_le_bytes_mod_order(&bytes));
+            }
+
+            res
+        }
+    }
+}
+
+mod round_nums{
+    // Adapted from https://github.com/filecoin-project/neptune/blob/master/src/round_numbers.rs
+
+    // The number of bits of the Poseidon prime field modulus. Denoted `n` in the Poseidon paper
+// (where `n = ceil(log2(p))`). Note that BLS12-381's scalar field modulus is 255 bits, however we
+// use 256 bits for simplicity when operating on bytes as the single bit difference does not affect
+// the round number security properties.
+    const PRIME_BITLEN: usize = 256;
+
+    // Security level (in bits), denoted `M` in the Poseidon paper.
+    const M: usize = 128;
+
+    /// The number of S-boxes (also called the "cost") given by equation (14) in the Poseidon paper:
+    /// `cost = t * R_F + R_P`.
+    fn n_sboxes(t: usize, rf: usize, rp: usize) -> usize {
+        t * rf + rp
+    }
+
+
+    /// Returns the round numbers for a given arity `(R_F, R_P)`.
+    pub fn round_numbers_base(arity: usize) -> (usize, usize) {
+        let t = arity + 1;
+        calc_round_numbers(t, true)
+    }
+
+    /// In case of newly-discovered attacks, we may need stronger security.
+    /// This option exists so we can preemptively create circuits in order to switch
+    /// to them quickly if needed.
+    ///
+    /// "A realistic alternative is to increase the number of partial rounds by 25%.
+    /// Then it is unlikely that a new attack breaks through this number,
+    /// but even if this happens then the complexity is almost surely above 2^64, and you will be safe."
+    /// - D Khovratovich
+    pub fn round_numbers_strengthened(arity: usize) -> (usize, usize) {
+        let (full_round, partial_rounds) = round_numbers_base(arity);
+
+        // Increase by 25%, rounding up.
+        let strengthened_partial_rounds = f64::ceil(partial_rounds as f64 * 1.25) as usize;
+
+        (full_round, strengthened_partial_rounds)
+    }
+
+    /// Returns the round numbers for a given width `t`. Here, the `security_margin` parameter does not
+    /// indicate that we are calculating `R_F` and `R_P` for the "strengthened" round numbers, done in
+    /// the function `round_numbers_strengthened()`.
+    pub(crate) fn calc_round_numbers(t: usize, security_margin: bool) -> (usize, usize) {
+        let mut rf = 0;
+        let mut rp = 0;
+        let mut n_sboxes_min = usize::MAX;
+
+        for mut rf_test in (2..=1000).step_by(2) {
+            for mut rp_test in 4..200 {
+                if round_numbers_are_secure(t, rf_test, rp_test) {
+                    if security_margin {
+                        rf_test += 2;
+                        rp_test = (1.075 * rp_test as f32).ceil() as usize;
+                    }
+                    let n_sboxes = n_sboxes(t, rf_test, rp_test);
+                    if n_sboxes < n_sboxes_min || (n_sboxes == n_sboxes_min && rf_test < rf) {
+                        rf = rf_test;
+                        rp = rp_test;
+                        n_sboxes_min = n_sboxes;
+                    }
+                }
+            }
+        }
+
+        (rf, rp)
+    }
+
+    /// Returns `true` if the provided round numbers satisfy the security inequalities specified in the
+    /// Poseidon paper.
+    fn round_numbers_are_secure(t: usize, rf: usize, rp: usize) -> bool {
+        let (rp, t, n, m) = (rp as f32, t as f32, PRIME_BITLEN as f32, M as f32);
+        let rf_stat = if m <= (n - 3.0) * (t + 1.0) {
+            6.0
+        } else {
+            10.0
+        };
+        let rf_interp = 0.43 * m + t.log2() - rp;
+        let rf_grob_1 = 0.21 * n - rp;
+        let rf_grob_2 = (0.14 * n - 1.0 - rp) / (t - 1.0);
+        let rf_max = [rf_stat, rf_interp, rf_grob_1, rf_grob_2]
+            .iter()
+            .map(|rf| rf.ceil() as usize)
+            .max()
+            .unwrap();
+        rf >= rf_max
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use ark_bls12_381::Fr;
+    use ark_ff::field_new;
+    use crate::crypto::hash::poseidon::constants::round_nums::calc_round_numbers;
+    use super::lfsr::GrainLFSR;
+    #[test]
+    fn test_grain_lfsr_consistency() {
+        // sage generate_parameters_grain_deterministic.sage 1 0 255 3 8 55 0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001
+
+        let mut lfsr = GrainLFSR::new(255, 3, 8, 55);
+        assert_eq!(
+            lfsr.get_field_elements_rejection_sampling::<Fr>(1)[0],
+            field_new!(
+                Fr,
+                "41764196652518280402801918994067134807238124178723763855975902025540297174931"
+            )
+        );
+        assert_eq!(
+            lfsr.get_field_elements_rejection_sampling::<Fr>(1)[0],
+            field_new!(
+                Fr,
+                "12678502092746318913289523392430826887011664085277767208266352862540971998250"
+            )
+        );
+    }
+
+    #[test]
+    fn test_round_numbers_against_known_values() {
+        // Each case contains a `t` (where `t = arity + 1`) and the `R_P` expected for that `t`.
+        let cases = [
+            (2usize, 55usize),
+            (3, 55),
+            (4, 56),
+            (5, 56),
+            (6, 56),
+            (7, 56),
+            (8, 57),
+            (9, 57),
+            (10, 57),
+            (11, 57),
+            (12, 57),
+            (13, 57),
+            (14, 57),
+            (15, 57),
+            (16, 59),
+            (17, 59),
+            (25, 59),
+            (37, 60),
+            (65, 61),
+        ];
+        for (t, rp_expected) in cases.iter() {
+            let (rf, rp) = calc_round_numbers(*t, true);
+            assert_eq!(rf, 8);
+            assert_eq!(rp, *rp_expected);
+        }
+    }
 }
