f64bcaf1e62962f60274a449e6c6dc37faf9fa40
Tom Shen <me@tomshen.io>
2022-06-23 14:06:40 -0700

    feat: mds security hardcoded test (#130)
    * feat: add security hardcoded tests for Poseidon mds matrices
    * feat: add sage script to generate those hardcoded tests
    
    Co-authored-by: Boyuan Feng <bfeng9@wisc.edu>

--- manta-pay/src/crypto/hash/poseidon/constants.rs
+++ manta-pay/src/crypto/hash/poseidon/constants.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with manta-rs.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Poseidon Permutation Parameters
+//! Poseidon Permutation Round Numbers
 
 use crate::crypto::hash::poseidon::Specification;
 
