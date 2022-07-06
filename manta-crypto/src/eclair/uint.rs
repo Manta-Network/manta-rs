cb4062f1241beb57395a2f175dbd8efae00b3d83
Boyuan Feng <bfeng9@wisc.edu>
2022-07-06 14:29:50 -0400

    feat: add UnsignedInteger

--- manta-crypto/src/eclair/uint.rs
+++ manta-crypto/src/eclair/uint.rs
@@ -21,27 +21,39 @@ use crate::{
     eclair::{assert::AssertWithinRange, ops::AddAssign},
 };
 
-/// Unsigned 128-bit Integer
-pub struct U128<T>(T);
+/// Unsigned Integer
+pub struct UnsignedInteger<T, const BITS: usize>(T);
 
-impl<T> U128<T> {
-    /// Builds a new [`U128`] over `value`, asserting that it does not exceed 128 bits.
+impl<T, const BITS: usize> UnsignedInteger<T, BITS> {
+    /// Builds a new [`UnsignedInteger`] over `value`, asserting that it does not exceed BITS bits.
     #[inline]
     pub fn new<COM>(value: T, compiler: &mut COM) -> Self
     where
-        COM: AssertWithinRange<T, 128>,
+        COM: AssertWithinRange<T, BITS>,
     {
         compiler.assert_within_range(&value);
         Self(value)
     }
+
+    /// Mutates an [`UnsignedInteger`] with a function `f`.
+    #[inline]
+    fn mutate<F, U, COM>(&mut self, f: F, compiler: &mut COM) -> U
+    where
+        COM: AssertWithinRange<T, BITS>,
+        F: FnOnce(&mut T, &mut COM) -> U,
+    {
+        let output = f(&mut self.0, compiler);
+        compiler.assert_within_range(&self.0);
+        output
+    }
 }
 
-impl<O, T, COM> Add<Self, COM> for U128<T>
+impl<T, COM, const BITS: usize> Add<Self, COM> for UnsignedInteger<T, BITS>
 where
-    COM: AssertWithinRange<O, 128>,
-    T: Add<T, COM, Output = O>,
+    COM: AssertWithinRange<T::Output, BITS>,
+    T: Add<T, COM>,
 {
-    type Output = U128<O>;
+    type Output = UnsignedInteger<T::Output, BITS>;
 
     #[inline]
     fn add(self, rhs: Self, compiler: &mut COM) -> Self::Output {
@@ -49,14 +61,27 @@ where
     }
 }
 
-impl<T, COM> AddAssign<Self, COM> for U128<T>
+impl<T, COM, const BITS: usize> AddAssign<Self, COM> for UnsignedInteger<T, BITS>
 where
-    COM: AssertWithinRange<T, 128>,
+    COM: AssertWithinRange<T, BITS>,
     T: AddAssign<T, COM>,
 {
     #[inline]
     fn add_assign(&mut self, rhs: Self, compiler: &mut COM) {
-        self.0.add_assign(rhs.0, compiler);
-        compiler.assert_within_range(&self.0);
+        self.mutate(|lhs, compiler| lhs.add_assign(rhs.0, compiler), compiler);
     }
+}
+
+macro_rules! define_uint {
+    ($($doc:expr, $name:ident, $bits:expr),* $(,)?) => {
+        $(
+            #[doc = $doc]
+            pub type $name<T> = UnsignedInteger<T, $bits>;
+        )*
+    }
+}
+
+define_uint! {
+    "64-Bit Unsigned Integer", U64, 64,
+    "128-Bit Unsigned Integer", U128, 128,
 }
