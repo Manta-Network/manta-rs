eb52bb7b3539b50f57a3a34e8e3eec6192ba471d
Brandon H. Gomes <bhgomes@pm.me>
2022-08-02 17:35:58 +0200

    wip: concrete implementation
    Signed-off-by: Brandon H. Gomes <bhgomes@pm.me>

--- manta-pay/src/config/utxo/v1.rs
+++ manta-pay/src/config/utxo/v1.rs
@@ -21,16 +21,20 @@ use crate::{
         poseidon::{
             Spec2 as Poseidon2, Spec3 as Poseidon3, Spec4 as Poseidon4, Spec5 as Poseidon5,
         },
-        ConstraintField, EmbeddedScalar, Group,
+        Compiler, ConstraintField, EmbeddedScalar, EmbeddedScalarVar, Group, GroupVar,
     },
     crypto::{
-        constraint::arkworks::{Boolean, Fp, R1CS},
+        constraint::arkworks::{Boolean, Fp, FpVar, R1CS},
         poseidon::{self, hash::Hasher, ParameterFieldType},
     },
 };
 use ark_ff::PrimeField;
 use core::marker::PhantomData;
-use manta_crypto::{eclair::Has, hash::ArrayHashFunction};
+use manta_crypto::{
+    eclair::{num::U128, Has},
+    hash::ArrayHashFunction,
+    merkle_tree,
+};
 
 pub use manta_accounting::transfer::utxo::v1 as protocol;
 
@@ -38,16 +42,37 @@ pub use manta_accounting::transfer::utxo::v1 as protocol;
 pub type AssetId = Fp<ConstraintField>;
 
 ///
+pub type AssetIdVar = FpVar<ConstraintField>;
+
+///
 pub type AssetValue = u128;
+
+///
+pub type AssetValueVar = U128<FpVar<ConstraintField>>;
 
 ///
 pub type ProofAuthorizationKey = Group;
 
 ///
+pub type ProofAuthorizationKeyVar = GroupVar;
+
+///
 pub type ViewingKey = EmbeddedScalar;
 
 ///
+pub type ViewingKeyVar = EmbeddedScalarVar;
+
+///
 pub type ReceivingKey = Group;
+
+///
+pub type ReceivingKeyVar = GroupVar;
+
+///
+pub type UtxoAccumulatorItem = Fp<ConstraintField>;
+
+///
+pub type UtxoAccumulatorItemVar = FpVar<ConstraintField>;
 
 ///
 pub struct UtxoCommitmentSchemeDomainTag;
@@ -87,6 +112,35 @@ impl protocol::UtxoCommitmentScheme for UtxoCommitmentScheme {
                 &Fp((*asset_value).into()),
                 &Fp(receiving_key.0.x),
                 &Fp(receiving_key.0.y),
+            ],
+            compiler,
+        )
+    }
+}
+
+impl protocol::UtxoCommitmentScheme<Compiler> for UtxoCommitmentScheme<Compiler> {
+    type AssetId = AssetIdVar;
+    type AssetValue = AssetValueVar;
+    type ReceivingKey = ReceivingKeyVar;
+    type Randomness = FpVar<ConstraintField>;
+    type Commitment = FpVar<ConstraintField>;
+
+    #[inline]
+    fn commit(
+        &self,
+        randomness: &Self::Randomness,
+        asset_id: &Self::AssetId,
+        asset_value: &Self::AssetValue,
+        receiving_key: &Self::ReceivingKey,
+        compiler: &mut Compiler,
+    ) -> Self::Commitment {
+        self.0.hash(
+            [
+                randomness,
+                asset_id,
+                asset_value.as_ref(),
+                &receiving_key.0.x,
+                &receiving_key.0.y,
             ],
             compiler,
         )
@@ -133,6 +187,20 @@ impl protocol::ViewingKeyDerivationFunction for ViewingKeyDerivationFunction {
     }
 }
 
+impl protocol::ViewingKeyDerivationFunction<Compiler> for ViewingKeyDerivationFunction<Compiler> {
+    type ProofAuthorizationKey = ProofAuthorizationKeyVar;
+    type ViewingKey = ViewingKeyVar;
+
+    #[inline]
+    fn viewing_key(
+        &self,
+        proof_authorization_key: &Self::ProofAuthorizationKey,
+        compiler: &mut Compiler,
+    ) -> Self::ViewingKey {
+        todo!()
+    }
+}
+
 ///
 pub type IncomingBaseEncryptionScheme<COM = ()> = poseidon::encryption::Encryption<Poseidon5, COM>;
 
@@ -158,7 +226,7 @@ impl protocol::UtxoAccumulatorItemHash for UtxoAccumulatorItemHash {
     type AssetId = AssetId;
     type AssetValue = AssetValue;
     type Commitment = Fp<ConstraintField>;
-    type Item = Fp<ConstraintField>;
+    type Item = UtxoAccumulatorItem;
 
     #[inline]
     fn hash(
@@ -180,6 +248,132 @@ impl protocol::UtxoAccumulatorItemHash for UtxoAccumulatorItemHash {
         )
     }
 }
+
+impl protocol::UtxoAccumulatorItemHash<Compiler> for UtxoAccumulatorItemHash<Compiler> {
+    type Bool = Boolean<ConstraintField>;
+    type AssetId = AssetIdVar;
+    type AssetValue = AssetValueVar;
+    type Commitment = FpVar<ConstraintField>;
+    type Item = UtxoAccumulatorItemVar;
+
+    #[inline]
+    fn hash(
+        &self,
+        is_transparent: &Self::Bool,
+        public_asset_id: &Self::AssetId,
+        public_asset_value: &Self::AssetValue,
+        commitment: &Self::Commitment,
+        compiler: &mut Compiler,
+    ) -> Self::Item {
+        self.0.hash(
+            [
+                &(is_transparent.clone()).into(),
+                public_asset_id,
+                public_asset_value.as_ref(),
+                commitment,
+            ],
+            compiler,
+        )
+    }
+}
+
+/// Leaf Hash Configuration Type
+pub type LeafHash = merkle_tree::IdentityLeafHash<UtxoAccumulatorItem>;
+
+/// Leaf Hash Variable Configuration Type
+pub type LeafHashVar = merkle_tree::IdentityLeafHash<UtxoAccumulatorItemVar, Compiler>;
+
+///
+pub struct InnerHashDomainTag;
+
+impl poseidon::hash::DomainTag<Poseidon2> for InnerHashDomainTag {
+    #[inline]
+    fn domain_tag() -> <Poseidon2 as ParameterFieldType>::ParameterField {
+        todo!()
+    }
+}
+
+/// Inner Hash Configuration
+pub struct InnerHash<COM = ()>(PhantomData<COM>);
+
+impl merkle_tree::InnerHash for InnerHash {
+    type LeafDigest = UtxoAccumulatorItem;
+    type Parameters = Hasher<Poseidon2, InnerHashDomainTag, 2>;
+    type Output = Fp<ConstraintField>;
+
+    #[inline]
+    fn join(
+        parameters: &Self::Parameters,
+        lhs: &Self::Output,
+        rhs: &Self::Output,
+        compiler: &mut (),
+    ) -> Self::Output {
+        parameters.hash([lhs, rhs], compiler)
+    }
+
+    #[inline]
+    fn join_leaves(
+        parameters: &Self::Parameters,
+        lhs: &Self::LeafDigest,
+        rhs: &Self::LeafDigest,
+        compiler: &mut (),
+    ) -> Self::Output {
+        parameters.hash([lhs, rhs], compiler)
+    }
+}
+
+impl merkle_tree::InnerHash<Compiler> for InnerHash<Compiler> {
+    type LeafDigest = UtxoAccumulatorItemVar;
+    type Parameters = Hasher<Poseidon2, InnerHashDomainTag, 2, Compiler>;
+    type Output = FpVar<ConstraintField>;
+
+    #[inline]
+    fn join(
+        parameters: &Self::Parameters,
+        lhs: &Self::Output,
+        rhs: &Self::Output,
+        compiler: &mut Compiler,
+    ) -> Self::Output {
+        parameters.hash([lhs, rhs], compiler)
+    }
+
+    #[inline]
+    fn join_leaves(
+        parameters: &Self::Parameters,
+        lhs: &Self::LeafDigest,
+        rhs: &Self::LeafDigest,
+        compiler: &mut Compiler,
+    ) -> Self::Output {
+        parameters.hash([lhs, rhs], compiler)
+    }
+}
+
+/// Merkle Tree Configuration
+pub struct MerkleTreeConfiguration;
+
+impl merkle_tree::HashConfiguration for MerkleTreeConfiguration {
+    type LeafHash = LeafHash;
+    type InnerHash = InnerHash;
+}
+
+impl merkle_tree::HashConfiguration<Compiler> for MerkleTreeConfiguration {
+    type LeafHash = LeafHashVar;
+    type InnerHash = InnerHash<Compiler>;
+}
+
+impl merkle_tree::Configuration for MerkleTreeConfiguration {
+    const HEIGHT: usize = 20;
+}
+
+impl merkle_tree::Configuration<Compiler> for MerkleTreeConfiguration {
+    const HEIGHT: usize = 20;
+}
+
+/// UTXO Accumulator Model
+pub type UtxoAccumulatorModel = merkle_tree::Parameters<MerkleTreeConfiguration>;
+
+/// UTXO Accumulator Model Variable
+pub type UtxoAccumulatorModelVar = merkle_tree::Parameters<MerkleTreeConfiguration, Compiler>;
 
 ///
 pub struct NullifierCommitmentSchemeDomainTag;
@@ -221,6 +415,29 @@ impl protocol::NullifierCommitmentScheme for NullifierCommitmentScheme {
     }
 }
 
+impl protocol::NullifierCommitmentScheme<Compiler> for NullifierCommitmentScheme<Compiler> {
+    type ProofAuthorizationKey = ProofAuthorizationKeyVar;
+    type UtxoAccumulatorItem = FpVar<ConstraintField>;
+    type Commitment = FpVar<ConstraintField>;
+
+    #[inline]
+    fn commit(
+        &self,
+        proof_authorization_key: &Self::ProofAuthorizationKey,
+        item: &Self::UtxoAccumulatorItem,
+        compiler: &mut Compiler,
+    ) -> Self::Commitment {
+        self.0.hash(
+            [
+                &proof_authorization_key.0.x,
+                &proof_authorization_key.0.y,
+                item,
+            ],
+            compiler,
+        )
+    }
+}
+
 ///
 pub type OutgoingBaseEncryptionScheme<COM = ()> = poseidon::encryption::Encryption<Poseidon2, COM>;
 
@@ -247,22 +464,22 @@ impl protocol::Configuration for Config {
 */
 
 /* TODO:
-impl<F> protocol::Configuration<R1CS<F>> for Config<R1CS<F>>
+impl<F> protocol::Configuration<Compiler> for Config<Compiler>
 where
     F: PrimeField,
 {
     type Bool = Boolean<F>;
-    type AssetId = ();
-    type AssetValue = ();
-    type Scalar = ();
-    type Group = ();
-    type UtxoCommitmentScheme = ();
-    type ViewingKeyDerivationFunction = ();
+    type AssetId = AssetIdVar;
+    type AssetValue = AssetValueVar;
+    type Scalar = EmbeddedScalarVar;
+    type Group = GroupVar;
+    type UtxoCommitmentScheme = UtxoCommitmentScheme<Compiler>;
+    type ViewingKeyDerivationFunction = ViewingKeyDerivationFunction<Compiler>;
     type IncomingCiphertext = ();
-    type IncomingBaseEncryptionScheme = ();
-    type UtxoAccumulatorItemHash = ();
+    type IncomingBaseEncryptionScheme = IncomingBaseEncryptionScheme<Compiler>;
+    type UtxoAccumulatorItemHash = UtxoAccumulatorItemHash<Compiler>;
     type UtxoAccumulatorModel = ();
     type OutgoingCiphertext = ();
-    type OutgoingBaseEncryptionScheme = ();
+    type OutgoingBaseEncryptionScheme = OutgoingBaseEncryptionScheme<Compiler>;
 }
 */
