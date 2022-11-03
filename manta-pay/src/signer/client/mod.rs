611bae3f16cf599b5eb55455803785337e8c4717
Alexander Parsh <53206800+AlexParshh@users.noreply.github.com>
2022-11-03 14:34:58 -0400

    Update manta-pay/src/signer/client/mod.rs
    Co-authored-by: Brandon H. Gomes <bhgomes@pm.me>
    Signed-off-by: Alexander Parsh <53206800+AlexParshh@users.noreply.github.com>

--- manta-pay/src/signer/client/mod.rs
+++ manta-pay/src/signer/client/mod.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with manta-rs.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Signer Websocket Client Implementation
+//! Signer Client
 
 #[cfg(feature = "websocket")]
 #[cfg_attr(doc_cfg, doc(cfg(feature = "websocket")))]
