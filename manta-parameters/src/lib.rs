5ce37e4d1995d5236074f99368c5ae09a26c28a5
Francisco Hernandez Iglesias <f.hernandeziglesias@outlook.com>
2022-11-30 13:27:04 +0100

    DEFAULT_BRANCH changed
    Signed-off-by: Francisco Hernandez Iglesias <f.hernandeziglesias@outlook.com>

--- manta-parameters/src/lib.rs
+++ manta-parameters/src/lib.rs
@@ -110,7 +110,7 @@ pub mod github {
     pub const CRATE: &str = "manta-parameters";
 
     /// Default GitHub Branch
-    pub const DEFAULT_BRANCH: &str = "feat/new-circuits";
+    pub const DEFAULT_BRANCH: &str = "feat/mantapay-v1";
 
     /// Returns the Git-LFS URL for GitHub content at the given `branch` and `data_path`.
     #[inline]
