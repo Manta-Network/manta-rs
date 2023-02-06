8230a55e2c6c7cc1d9c0fe6c06997473ba6fbcc8
SupremoUGH <francisco@manta.network>
2023-02-06 19:21:09 +0100

    docs

--- manta-accounting/src/wallet/signer/functions.rs
+++ manta-accounting/src/wallet/signer/functions.rs
@@ -883,7 +883,8 @@ where
     }
 }
 
-/// Signs the `transaction`, generating transfer posts and returning their [`TransactionData`].
+/// Signs the `transaction`, generating transfer posts 
+/// and returning their [`TransactionData`].
 #[inline]
 pub fn sign_with_transaction_data<C>(
     parameters: &SignerParameters<C>,
@@ -895,7 +896,7 @@ pub fn sign_with_transaction_data<C>(
 ) -> SignWithTransactionDataResult<C>
 where
     C: Configuration,
-    TransferPost<C>: Clone,
+    TransferPost<C>: Clone
 {
     Ok(SignWithTransactionDataResponse(
         sign(
