7b11aeb4e1f9ef724cf6fb8d5aa071da4df1ab5d
SupremoUGH <francisco@manta.network>
2023-02-08 19:40:08 +0100

    bench added

--- manta-benchmark/www/index.js
+++ manta-benchmark/www/index.js
@@ -1,55 +1,52 @@
-import { Context, prove_to_private, prove_private_transfer, prove_to_public} from "wasm-prover";
+import {
+  Context,
+  prove_to_private,
+  prove_private_transfer,
+  prove_to_public,
+} from "wasm-prover";
+import { buildPoseidon } from "circomlibjs/src/poseidon_wasm.js";
 
 const pre = document.getElementById("wasm-prover");
 
 const REPEAT = 5;
 
 // Computes the median of an array
-const median = arr => {
-    const mid = Math.floor(arr.length / 2),
-      nums = [...arr].sort((a, b) => a - b);
-    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
-  };
+const median = (arr) => {
+  const mid = Math.floor(arr.length / 2),
+    nums = [...arr].sort((a, b) => a - b);
+  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+};
 
 function bench_prove_to_private() {
   const context = new Context();
-  const perf = Array.from(
-      {length: REPEAT},
-      (_, i) => {
-        const t0 = performance.now();
-        prove_to_private(context);
-        const t1 = performance.now();
-        return t1 - t0;
-      }
-  );
+  const perf = Array.from({ length: REPEAT }, (_, i) => {
+    const t0 = performance.now();
+    prove_to_private(context);
+    const t1 = performance.now();
+    return t1 - t0;
+  });
   return `prove to_private performance: ${median(perf)} ms \n`;
 }
 
 function bench_prove_private_transfer() {
   const context = new Context();
-  const perf = Array.from(
-      {length: REPEAT},
-      (_, i) => {
-        const t0 = performance.now();
-        prove_private_transfer(context);
-        const t1 = performance.now();
-        return t1 - t0;
-      }
-  );
+  const perf = Array.from({ length: REPEAT }, (_, i) => {
+    const t0 = performance.now();
+    prove_private_transfer(context);
+    const t1 = performance.now();
+    return t1 - t0;
+  });
   return `prove private transfer performance: ${median(perf)} ms \n`;
 }
 
 function bench_prove_to_public() {
   const context = new Context();
-  const perf = Array.from(
-      {length: REPEAT},
-      (_, i) => {
-        const t0 = performance.now();
-        prove_to_public(context);
-        const t1 = performance.now();
-        return t1 - t0;
-      }
-  );
+  const perf = Array.from({ length: REPEAT }, (_, i) => {
+    const t0 = performance.now();
+    prove_to_public(context);
+    const t1 = performance.now();
+    return t1 - t0;
+  });
   return `prove to_public performance: ${median(perf)} ms \n`;
 }
 
@@ -61,3 +58,20 @@ pre.textContent += bench_prove_private_transfer();
 
 // benchmarks proof time for to_public
 pre.textContent += bench_prove_to_public();
+
+function bench_poseidon_js() {
+  let poseidonWasm = buildPoseidon();
+  const inp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  const st = 0;
+  const nOut = 17;
+  const perf = Array.from({ length: REPEAT }, (_, i) => {
+    const t0 = performance.now();
+    poseidonWasm(inp, st, nOut);
+    const t1 = performance.now();
+    return t1 - t0;
+  });
+  return `poseidon_js performance: ${median(perf)} ms \n`;
+}
+
+// benchmarks proof for poseidon hash
+pre.textContent += bench_poseidon_js();
